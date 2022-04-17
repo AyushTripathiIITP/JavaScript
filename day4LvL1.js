308da3f5c97702cdbf47987cd109e2276f094b26
AyushTripathiIITP <101452500+AyushTripathiIITP@users.noreply.github.com>
2022-04-17 11:15:30 +0530

    Add files via upload

---
+++ day4LvL1.js
@@ -0,0 +1,48 @@
+var age = prompt("Enter your age")
+if( age < 18){
+    print("To young bro")
+}
+else if(age >= 18){
+    print("Lets get rollin' vro")
+}
+var mon = prompt("Enter a month")
+mon.toLowerCase()
+print(mon)
+if(mon == "january"){
+    alert("january has 31 days")
+}
+else if(mon == "febuary"){
+    alert("feb has 28 days")
+}
+else if(mon == "march"){
+    alert("march has 31 days")
+}
+else if(mon == "april"){
+    alert("april has 30 days")
+}
+else if(mon == "may"){
+    alert("may has 31 days")
+
+}
+else if(mon == "june"){
+    alert("june has 30 days")
+
+}
+else if(mon == "july"){
+    alert("july has 31 days")
+}
+else if(mon == "august"){
+    alert("august has 31 days")
+}
+else if(mon == "september"){
+    alert("september has 30 days")
+}
+else if(mon == "october"){
+    alert("october has 31 days")
+}
+else if(mon == "november"){
+    alert("nov has 30 days")
+}
+else if(mon == "december"){
+    alert("dec has 31 days")
+}
